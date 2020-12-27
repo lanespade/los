d58249e70418029796f0119d81286eedf9e6a877
Lane Spade <5313372+lanespade@users.noreply.github.com>
2020-12-27 14:07:22 -0800

    Replaced shiftit with rectangle and removed 'cask' from brew commands

--- brew.sh
+++ brew.sh
@@ -5,11 +5,11 @@ echo 'Installing homebrew'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo 'Installing casks'
-brew cask reinstall shiftit
-brew cask reinstall vlc
-brew cask reinstall google-backup-and-sync
-brew cask reinstall webtorrent
-brew cask reinstall homebrew/cask-drivers/sony-ps4-remote-play
-brew cask reinstall openemu
-brew cask reinstall expressvpn
-brew cask reinstall google-chrome
+brew reinstall rectangle
+brew reinstall vlc
+brew reinstall google-backup-and-sync
+brew reinstall webtorrent
+brew reinstall homebrew/cask-drivers/sony-ps4-remote-play
+brew reinstall openemu
+brew reinstall expressvpn
+brew reinstall google-chrome
