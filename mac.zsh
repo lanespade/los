40286971bcde5cc11700aaa0e54e57137939a004
Lane Spade <5313372+lanespade@users.noreply.github.com>
2021-08-03 00:16:39 -0700

    Switched defaults commands to use proper types

--- mac.zsh
+++ mac.zsh
@@ -12,10 +12,10 @@ sudo dscl . delete "/Users/$USER" JPEGPhoto
 sudo dscl . create "/Users/$USER" Picture "$PWD/avatar.jpg"
 
 echo 'Configuring dock'
-defaults write com.apple.dock static-only -bool true
+defaults write com.apple.dock static-only -bool TRUE
 defaults write com.apple.dock tilesize -int 128
-defaults write com.apple.dock autohide -int 1
-defaults write com.apple.dock show-recents -int 0
+defaults write com.apple.dock autohide -bool TRUE
+defaults write com.apple.dock show-recents -bool FALSE
 killall Dock
 
 echo 'Configuring keyboard'
@@ -23,6 +23,6 @@ defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 
 echo 'Configuring track pad'
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
-defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
-defaults write -g com.apple.swipescrolldirection -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool TRUE
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool TRUE
+defaults write -g com.apple.swipescrolldirection -bool FALSE
