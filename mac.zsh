27bb5c0abedd497c4bab855ac110d5f971377c9a
Lane Spade <5313372+lanespade@users.noreply.github.com>
2021-08-04 22:04:40 -0700

    Flushed entire preference cache and added showing bluetooth in menu bar

--- mac.zsh
+++ mac.zsh
@@ -1,9 +1,31 @@
 #!/bin/zsh
 set -Eeuo pipefail
 
-echo 'Configuring theme'
+echo 'Configuring global preferences: theme, keyboard, and scroll direction'
 plutil -replace AppleInterfaceStyle -string Dark ~/Library/Preferences/.GlobalPreferences.plist
-defaults read -g AppleInterfaceStyle -string Dark
+plutil -replace InitialKeyRepeat -integer 10 ~/Library/Preferences/.GlobalPreferences.plist
+plutil -replace KeyRepeat -integer 1 ~/Library/Preferences/.GlobalPreferences.plist
+plutil -replace "com\.apple\.swipescrolldirection" -bool false ~/Library/Preferences/.GlobalPreferences.plist
+
+echo 'Control center preferences'
+plutil -replace "NSStatusItem Visible Bluetooth" -bool true ~/Library/Preferences/com.apple.controlcenter.plist
+
+echo 'Configuring dock preferences'
+plutil -replace static-only -bool true ~/Library/Preferences/com.apple.dock.plist
+plutil -replace tilesize -integer 128 ~/Library/Preferences/com.apple.dock.plist
+plutil -replace autohide -bool true ~/Library/Preferences/com.apple.dock.plist
+plutil -replace show-recents -bool false ~/Library/Preferences/com.apple.dock.plist
+
+echo 'Configuring track pad preferences'
+plutil -replace Clicking -bool true ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist
+plutil -replace TrackpadRightClick -bool true ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist
+
+echo 'Configuring bluetooth track pad preferences'
+plutil -replace Clicking -bool true ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist
+plutil -replace TrackpadRightClick -bool true ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist
+
+echo 'Flush prefence cache'
+defaults read > /dev/null
 
 echo 'Configuring background image'
 osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$PWD/background.jpg\""
@@ -11,33 +33,3 @@ osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \
 echo 'Configuring profile photo, you may be asked for your password'
 sudo dscl . delete "/Users/$USER" JPEGPhoto
 sudo dscl . create "/Users/$USER" Picture "$PWD/avatar.jpg"
-
-echo 'Configuring dock'
-plutil -replace static-only -bool true ~/Library/Preferences/com.apple.dock.plist
-defaults read com.apple.dock static-only
-
-plutil -replace tilesize -integer 128 ~/Library/Preferences/com.apple.dock.plist
-defaults read com.apple.dock tilesize
-
-plutil -replace autohide -bool true ~/Library/Preferences/com.apple.dock.plist
-defaults read com.apple.dock autohide
-
-plutil -replace show-recents -bool false ~/Library/Preferences/com.apple.dock.plist
-defaults read com.apple.dock show-recents
-
-echo 'Configuring keyboard'
-plutil -replace InitialKeyRepeat -integer 10 ~/Library/Preferences/.GlobalPreferences.plist
-defaults read -g InitialKeyRepeat
-
-plutil -replace KeyRepeat -integer 1 ~/Library/Preferences/.GlobalPreferences.plist
-defaults read -g KeyRepeat
-
-echo 'Configuring track pad'
-plutil -replace Clicking -bool true ~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad.plist
-defaults read com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking
-
-plutil -replace Clicking -bool true ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist
-defaults read com.apple.AppleMultitouchTrackpad Clicking
-
-plutil -replace "com\.apple\.swipescrolldirection" -bool false ~/Library/Preferences/.GlobalPreferences.plist
-defaults read -g com.apple.swipescrolldirection 
